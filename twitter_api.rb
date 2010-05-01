6cc84a190b05154b5826a2dc86c299564f9e9e5d
Benjamin Fritsch <beanie@benle.de>
2010-05-01 22:56:05 +0200

    ready to go :)

--- twitter_api.rb
+++ twitter_api.rb
@@ -8,17 +8,10 @@ require 'yaml'
 config = YAML.load_file("twitter.yml")
 USERNAME = config["twitter"]["username"]
 PASSWORD = config["twitter"]["password"]
-KEYWORDS = ['ichtest']
+KEYWORDS = ['nowplaying']
 
 class TwitterApi
-  attr_accessor :username, :password
-  
-  def initialize(username, password)
-    @username = username
-    @password = password
-  end
-  
-  def go!
+  def self.go!
     AMQP.start(:host => 'localhost') do
 
       twitter_queue = MQ.new.fanout('twitter')
@@ -36,16 +29,15 @@ class TwitPic
   def self.analyze(text)
     scan_for_twitpic = text.scan(/(http:\/\/twitpic.com\/)(\w*)/)
     images = []
-    
+
     unless scan_for_twitpic.empty?
       scan_for_twitpic.each do |twitpic|
         images << "http://twitpic.com/show/full/" + twitpic.last
       end
     end
-    
+
     images
   end
 end
 
-api = TwitterApi.new(USERNAME,PASSWORD)
-api.go!
+TwitterApi.go!
